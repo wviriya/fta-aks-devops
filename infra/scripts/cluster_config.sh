5932cfd1dd06723809e54d2f86b3ad9b997975af
wviriya <wviriya@MININT-E92EP2J.southpacific.corp.microsoft.com>
2020-05-27 15:38:59 +1200

    first commit

---
+++ infra/scripts/cluster_config.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+SUBSCRIPTION_ID=$1
+RESOURCE_GROUP=$2
+CLUSTER_NAME=$3
+NAME_SPACES=("test" "uat" "prod")
+IDENTITY_NAME="${3}-podid"
+
+az aks get-credentials -n $CLUSTER_NAME -g $RESOURCE_GROUP
+
+for namespace in ${NAME_SPACES[@]}; do
+     # create namespace
+     kubectl create namespace $namespace
+done
+
+# Deploy aad-pod-identity
+helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
+helm install aad-pod-identity aad-pod-identity/aad-pod-identity
+
+# Install secrets store csi driver and Azure Key Vault Provider
+helm repo add secrets-store-csi-driver https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/master/charts
+helm install csi-secrets-store secrets-store-csi-driver/secrets-store-csi-driver
+
+az identity create -g $RESOURCE_GROUP -n $IDENTITY_NAME --subscription $SUBSCRIPTION_ID
+
+IDENTITY_CLIENT_ID="$(az identity show -g $RESOURCE_GROUP -n $IDENTITY_NAME --subscription $SUBSCRIPTION_ID --query clientId -otsv)"
+IDENTITY_RESOURCE_ID="$(az identity show -g $RESOURCE_GROUP -n $IDENTITY_NAME --subscription $SUBSCRIPTION_ID --query id -otsv)"
+IDENTITY_ASSIGNMENT_ID="$(az role assignment create --role Reader --assignee $IDENTITY_CLIENT_ID --scope /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP --query id -otsv)"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: "aadpodidentity.k8s.io/v1"
+kind: AzureIdentity
+metadata:
+  name: $IDENTITY_NAME
+spec:
+  type: 0
+  resourceID: $IDENTITY_RESOURCE_ID
+  clientID: $IDENTITY_CLIENT_ID
+EOF
+
+cat <<EOF | kubectl apply -f -
+apiVersion: "aadpodidentity.k8s.io/v1"
+kind: AzureIdentityBinding
+metadata:
+  name: $IDENTITY_NAME-binding
+spec:
+  azureIdentity: $IDENTITY_NAME
+  selector: $IDENTITY_NAME
+EOF
+
+
+
